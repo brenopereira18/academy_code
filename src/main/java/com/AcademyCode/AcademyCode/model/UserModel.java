651abbca91893af11caf333d87a9946c16e7bc33
brenopereira18 <brenopereira@gmail.com>
2025-01-08 20:53:03 -0300

    :sparkles: feat: create entity model

---
+++ src/main/java/com/AcademyCode/AcademyCode/model/UserModel.java
@@ -0,0 +1,40 @@
+package com.AcademyCode.AcademyCode.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank()
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço.")
+    @Column(unique = true, length = 30, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @Length(min = 8, max = 100, message = "A senha deve conter entre 8 a 100 caracteres.")
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
