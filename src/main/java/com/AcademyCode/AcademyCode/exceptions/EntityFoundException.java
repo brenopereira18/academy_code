246a677c5b37eb99b5a8318153193df614ba90f7
brenopereira18 <brenopereira@gmail.com>
2025-01-16 17:25:54 -0300

    :sparkles: feat: create custom exceptions

---
+++ src/main/java/com/AcademyCode/AcademyCode/exceptions/EntityFoundException.java
@@ -0,0 +1,7 @@
+package com.AcademyCode.AcademyCode.exceptions;
+
+public class EntityFoundException extends RuntimeException{
+    public EntityFoundException(String message) {
+        super(message);
+    }
+}
