f51826cf3276b6193aee2078dce6fa7d7f97b3dc
brenopereira18 <brenopereira@gmail.com>
2025-01-20 15:03:01 -0300

    :sparkles: feat: response to crud request

---
+++ src/main/java/com/AcademyCode/AcademyCode/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.AcademyCode.AcademyCode.controller;
+
+import com.AcademyCode.AcademyCode.Service.UserService;
+import com.AcademyCode.AcademyCode.model.UserModel;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<UserModel> create(@Valid @RequestBody UserModel userModel ) {
+        UserModel user = userService.create(userModel);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> users = userService.getAllUsers();
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserModel> update(@Valid @PathVariable UUID id, @RequestBody UserModel userModel) {
+        UserModel updatedUser = userService.update(id, userModel);
+        return ResponseEntity.ok().body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
