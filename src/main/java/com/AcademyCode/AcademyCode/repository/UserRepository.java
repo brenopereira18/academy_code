2eb26d95a19822c8ec9e6e82beb7da5cfee11248
brenopereira18 <brenopereira@gmail.com>
2025-01-21 16:59:15 -0300

    :sparkles: feat: new search by user

--- src/main/java/com/AcademyCode/AcademyCode/repository/UserRepository.java
+++ src/main/java/com/AcademyCode/AcademyCode/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.AcademyCode.AcademyCode.repository;
 
 import com.AcademyCode.AcademyCode.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +10,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     Optional<UserModel> findById(UUID uuid);
     Optional<UserModel> findByUsername(String username);
+    UserDetails findByLogin(String login);
 }
