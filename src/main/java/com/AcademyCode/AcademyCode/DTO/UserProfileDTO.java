577f315203ea97bf710e06d01933e777ab1a60ec
brenopereira18 <brenopereira@gmail.com>
2025-01-22 16:18:53 -0300

    :sparkles: feat: create dto to update user profile

---
+++ src/main/java/com/AcademyCode/AcademyCode/DTO/UserProfileDTO.java
@@ -0,0 +1,11 @@
+package com.AcademyCode.AcademyCode.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserProfileDTO {
+    private String name;
+    private String password;
+}
