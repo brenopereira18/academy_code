3fa85fff24453d314dee0295ffa4769bb2a165d9
brenopereira18 <brenopereira@gmail.com>
2025-02-11 11:07:28 -0300

    :sparkles: feat: add new annotations for class

--- src/main/java/com/AcademyCode/AcademyCode/modules/course/model/CourseModel.java
+++ src/main/java/com/AcademyCode/AcademyCode/modules/course/model/CourseModel.java
@@ -5,7 +5,10 @@ import com.AcademyCode.AcademyCode.modules.course.enums.Status;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,6 +18,9 @@ import java.util.UUID;
 @Data
 @Entity(name = "course")
 @Table(name = "course")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseModel {
 
     @Id
