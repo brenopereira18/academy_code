86e7281a6801cb0478f1a165913b66944ac34d6e
brenopereira18 <brenopereira@gmail.com>
2025-02-17 22:53:31 -0300

    :recycle: refactor: add annotation

--- src/main/java/com/AcademyCode/AcademyCode/modules/user/DTO/AuthenticationUserDTO.java
+++ src/main/java/com/AcademyCode/AcademyCode/modules/user/DTO/AuthenticationUserDTO.java
@@ -2,10 +2,12 @@ package com.AcademyCode.AcademyCode.modules.user.DTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AuthenticationUserDTO {
 
     @Schema(description = "Username do usuário", example = "joão_silva")
